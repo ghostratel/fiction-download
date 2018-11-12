0548e006012cce217aae29aff7ba21a6e40f40f6
fenggang <1353640486@qq.com>
2018-11-12 17:31:27 +0800

    mongoose 加入数据类型校验

--- server/index.js
+++ server/index.js
@@ -1,64 +1,66 @@
-const Koa = require('koa')
-const Router = require('koa-router')
-const render = require('koa-art-template')
-const path = require('path')
-const DB = require('./modules/DB.js')
-const bodyParser = require('koa-bodyparser')
-const db = DB.getInstance()
+const Koa = require("koa");
+const Router = require("koa-router");
+const render = require("koa-art-template");
+const path = require("path");
+const DB = require("./modules/DB.js");
+const bodyParser = require("koa-bodyparser");
+const db = DB.getInstance();
 
-const app = new Koa()
+const app = new Koa();
 
-
-const router = new Router()
+const router = new Router();
 
 render(app, {
-    root: __dirname + '/views',
-    extname: '.art',
-    debug: process.env.NODE_ENV !== 'production'
+    root: __dirname + "/views",
+    extname: ".art",
+    debug: process.env.NODE_ENV !== "production"
 });
 
+app.use(require("koa-static")(__dirname + "/static"));
+app.use(bodyParser());
 
-app.use(require('koa-static')(__dirname + '/static'))
-app.use(bodyParser())
+router.get("/", async (ctx, next) => {
+    let users = await db.find();
+    await ctx.render("index", { users: users });
+    await next();
+});
 
+router.get("/edit", async (ctx, next) => {
+    let { _id } = ctx.request.query;
+    let user = await db.find({ _id: _id });
+    await ctx.render("edit", { user: user[0] });
+    await next();
+});
 
-router.get('/', async (ctx, next) => {
-    let users = await db.find()
-    await ctx.render('index', {users: users})
-    await next()
-})
+router.get("/delete", async (ctx, next) => {
+    let { _id } = ctx.request.query;
+    await db.deleteOne({ _id: _id });
+    ctx.redirect("/");
+    await next();
+});
 
-router.get('/edit', async (ctx, next) => {
-    let {_id} = ctx.request.query
-    let user = await db.find({'_id': _id})
-    await ctx.render('edit', {user: user[0]})
-    await next()
-})
+router.post("/add", async (ctx, next) => {
+    let user = ctx.request.body;
+    try {
+        await db.insertOne(user);
+    } catch (er) {
+        ctx.body = "参数不完整";
+        return
+    }
+    ctx.redirect("/");
+    await next();
+});
 
-router.get('/delete', async (ctx, next) => {
-    let {_id} = ctx.request.query
-    await db.deleteOne({'_id': _id})
-    ctx.redirect('/')
-    await next()
-})
+router.post("/update", async (ctx, next) => {
+    let user = ctx.request.body;
+    await db.updateOne(
+        { _id: user._id },
+        { $set: { name: user.name, age: user.age, from: user.from } }
+    );
+    ctx.redirect("/");
+    await next();
+});
 
-router.post('/add', async (ctx, next) => {
-    let user = ctx.request.body
-    await db.insertOne(user)
-    ctx.redirect('/')
-    await next()
-})
+app.use(router.routes()).use(router.allowedMethods());
 
-router.post('/update', async (ctx, next) => {
-    let user = ctx.request.body
-    await db.updateOne({'_id': user._id}, {$set: {name: user.name, age: user.age, from: user.from}})
-    ctx.redirect('/')
-    await next()
-})
-
-
-app
-    .use(router.routes())
-    .use(router.allowedMethods())
-
-app.listen(9527)
+app.listen(9527);
--- server/modules/DB.js
+++ server/modules/DB.js
@@ -1,64 +1,77 @@
-const mongoose = require("mongoose")
-const { MongoDBURL, DBname, DBUsername, DBPassword } = require("./config.js")
-const EventEmitter = require("events")
-const connect = Symbol("connect")
+const mongoose = require("mongoose");
+const { MongoDBURL, DBname, DBUsername, DBPassword } = require("./config.js");
+const EventEmitter = require("events");
+const connect = Symbol("connect");
 
 class DB extends EventEmitter {
     constructor() {
-        super()
-        this[connect]()
+        super();
+        this[connect]();
     }
 
     static getInstance() {
         if (!this.instance) {
-            this.instance = new DB()
+            this.instance = new DB();
         }
-        return this.instance
+        return this.instance;
     }
 
     [connect]() {
         mongoose.connect(
             MongoDBURL + DBname,
             { user: DBUsername, pass: DBPassword, useNewUrlParser: true }
-        )
-        this.HeroSchema = mongoose.Schema({name: String, age: Number, from: String})
-        this.HeroModel = mongoose.model("userinfo", this.HeroSchema, "userinfo")
+        );
+        this.HeroSchema = mongoose.Schema({
+            name: { type: String, trim: true, required: true },
+            age: {
+                type: Number,
+                set(value) {
+                    return value < 0 ? 0 : value;
+                }
+            },
+            from: { type: String, trim: true }
+        });
+        this.HeroModel = mongoose.model(
+            "userinfo",
+            this.HeroSchema,
+            "userinfo"
+        );
     }
 
     find(query = {}, options) {
         return new Promise((resolve, reject) => {
             this.HeroModel.find(query, options, (err, docs) => {
-                err ? reject(err) : resolve(docs)
-            })
-        })
+                err ? reject(err) : resolve(docs);
+            });
+        });
     }
 
     insertOne(doc) {
-        return new this.HeroModel(doc).save()
+        return new this.HeroModel(doc).save();
     }
 
     updateOne(query, update) {
         return new Promise((resolve, reject) => {
             this.HeroModel.updateOne(query, update, (err, result) => {
-                err ? reject(err) : resolve(result)
-            })
-        })
+                err ? reject(err) : resolve(result);
+            });
+        });
     }
 
     deleteOne(query) {
         return new Promise((resolve, reject) => {
             this.HeroModel.deleteOne(query, (err, result) => {
-                err ? reject(err) : resolve(result)
-            })
-        })
+                err ? reject(err) : resolve(result);
+            });
+        });
     }
 
     close() {
         return new Promise(resolve => {
-            this.client.close(resolve)
-            this.emit("close")
-        })
+            this.client.close(resolve);
+            this.emit("close");
+        });
     }
 }
 
-module.exports = DB
+module.exports = DB;
