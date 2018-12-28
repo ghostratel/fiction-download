9abfbad90e1483b5b0630b2aee97a362be50f4a4
fenggang <1353640486@qq.com>
2018-12-28 16:07:16 +0800

    1、添加iconfont字体图标

---
+++ browser/h5/src/pages/bookcase/index.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class PageSearch extends Component {
+	render(){
+		return (
+			<div>
+				bookcase
+			</div>
+		)
+	}
+}
+
+export default PageSearch
---
+++ browser/h5/src/pages/search/index.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class PageSearch extends Component {
+	render(){
+		return (
+			<div>
+				search
+			</div>
+		)
+	}
+}
+
+export default PageSearch
