b05a82a182e173ef47d9006cb0d8ad8cc011db2c
fenggang <1353640486@qq.com>
2018-12-28 16:13:07 +0800

    reformat

--- server/routes/novel/categories.js
+++ server/routes/novel/categories.js
@@ -3,24 +3,26 @@ const categories = require('../../crawler/categories.js')
 const { responseWrapper } = require('../../modules/utils.js')
 
 for (let category of categories) {
-    delete category.startPage
-    delete category.done
+	delete category.startPage
+	delete category.done
 }
 
 router.get('/categories', (req, res) => {
-    res.send(responseWrapper({
-        code: 1,
-        data: categories
-    }))
+	res.send(
+		responseWrapper({
+			code: 1,
+			data: categories
+		})
+	)
 })
 
 router.get('/categories/:cateID', (req, res) => {
-    let { cateID } = req.params
-    if (categories.find(category => category.cateID === cateID)) {
-        res.send(responseWrapper({ code: 1, data: cateID }))
-    } else {
-        res.send(responseWrapper({ code: 0, data: '不存在此分类!' }))
-    }
+	let { cateID } = req.params
+	if (categories.find(category => category.cateID === cateID)) {
+		res.send(responseWrapper({ code: 1, data: cateID }))
+	} else {
+		res.send(responseWrapper({ code: 0, data: '不存在此分类!' }))
+	}
 })
 
 module.exports = router
--- server/routes/novel/index.js
+++ server/routes/novel/index.js
@@ -1,7 +1,6 @@
 const router = require('express').Router()
 const categories = require('./categories.js')
 
-
 router.use('/novel', categories)
 
 module.exports = router
