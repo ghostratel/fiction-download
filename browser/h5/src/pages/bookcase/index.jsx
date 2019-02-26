5a004da46501ddfa204a41c94abd7b2dbd3f6b9d
fenggang <1353640486@qq.com>
2019-02-26 16:20:55 +0800

    路由组件过渡动画

--- browser/h5/src/pages/bookcase/index.jsx
+++ browser/h5/src/pages/bookcase/index.jsx
@@ -1,13 +1,33 @@
 import React, { Component } from 'react'
+import {CSSTransition} from 'react-transition-group'
 import './index.scss'
 
 class PageSearch extends Component {
+	constructor(props){
+		super(props)
+		this.state = {
+			show: false
+		}
+		this.toggleShow = this.toggleShow.bind(this)
+	}
 	render(){
+		const {show} = this.state
 		return (
-			<div id='dd'>
-				按时大多数按时大多数按时大多数按时大多数按时大多数按时大多数按时大多数按时大多数按时大多数按时大多数按时大多数按时大多数按时大多数按时大多数按时大多数按时大多数按时大多数按时大多数按时大多数按时大多数按时大多数按时大多数按时大多数按时大多数按时大多数按时大多数按时大多数按时大多数按时大多数按时大多数按时大多数按时大多数按时大多数按时大多数按时大多数按时大多数按时大多数按时大多数按时大多数按时大多数
+			<div className='bookcase'>
+				<button onClick={this.toggleShow}>
+					click!
+				</button>
+				<CSSTransition in={show} timeout={300} classNames='message'>
+					<div>
+						66666666666666666
+					</div>
+				</CSSTransition>
 			</div>
 		)
+	}
+	toggleShow(){
+		this.setState({show: !this.state.show})
+		console.log(this.state.show);
 	}
 }
 
--- browser/h5/src/pages/booklist/index.jsx
+++ browser/h5/src/pages/booklist/index.jsx
@@ -1,22 +1,31 @@
 import React, {Component} from 'react'
+import {CSSTransition} from 'react-transition-group'
 import './index.scss'
 
 class PageBooklist extends Component {
 	constructor(props){
 		super(props)
 		this.state = {
-			cateID: null
+			cateID: null,
+			pageEnter: false
 		}
+		this.back = this.back.bind(this)
 	}
 	componentDidMount(){
-		this.setState({cateID: this.props.match.params.cateID})
+		this.setState({cateID: this.props.match.params.cateID, pageEnter: true})
 	}
 	render(){
+		const {pageEnter} = this.state
 		return (
-			<div className='booklist'>
-				{this.state.cateID}
-			</div>
+			<CSSTransition classNames='fade' in={pageEnter} timeout={400}>
+				<div className='booklist' onClick={this.back}>
+					{this.state.cateID}
+				</div>
+			</CSSTransition>
 		)
+	}
+	back(){
+		this.props.history.go(-1)
 	}
 }
 
